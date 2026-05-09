dd7d54f0f0bc6232b18452f64f6c723eff490854
Dev Who BUIDL <devwhobuidl@gmail.com>
2026-05-10 02:43:08 +0400

    feat: push profile header component with missing import fix

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { updateProfile } from "@/app/actions/user";
